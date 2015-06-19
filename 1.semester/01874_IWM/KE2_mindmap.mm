40b08d0b2875fc3bc3d6ce5740bb62536b1db65e
Alexander <alexander.albrant@googlemail.com>
2015-06-19 23:29:40 +0200

    add new stuff ke2
    aufgabe 1 und  mindmap für ke2

---
+++ 1.semester/01874_IWM/KE2_mindmap.mm
@@ -0,0 +1,247 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1434483374510" ID="ID_1796353075" MODIFIED="1434483404889" TEXT="KE2">
+<node CREATED="1434483443533" ID="ID_1786052633" MODIFIED="1434483462284" POSITION="right" TEXT="1. Ontologie basierendes Informationsmanagement">
+<node CREATED="1434484951870" ID="ID_1283368788" MODIFIED="1434484961505" TEXT="1.1 Semantische Heterogenit&#xe4;t">
+<node CREATED="1434485161266" ID="ID_775353498" MODIFIED="1434485248684" TEXT="semantische aquivalente Konzepte">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bei semantisch &#228;quivalenten Konzepten k&#246;nnen sich etwa verschiedene Begriffe auf ein und dasselbe Konzept beziehen oder verschiedene Eigenschaften f&#252;r das gleiche Produkt von zwei Systemen modelliert werden.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434485173932" ID="ID_1260039672" MODIFIED="1434485323589" TEXT="sematische unabh&#xe4;ngige Konzepte">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bei semantisch unabh&#228;ngigen Konzepten kann es zu Verst&#228;ndnis-problemen kommen, wenn der gleiche Begriff f&#252;r unterschiedliche Konzepte verwendet wird. ls Beispiel kann &#8222;<i>K&#228;fer</i>&#8220; entweder f&#252;r ein &#8222;<i>Tier</i>&#8220; oder f&#252;r ein &#8222;<i>Auto</i>&#8220; stehen.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434485186954" ID="ID_1493538189" MODIFIED="1434485368704" TEXT="semantische abh&#xe4;ngige Konzepte">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bei semantisch abh&#228;ngigen Konzepten wird die Heterogenit&#228;t anhand von Generalisierung und Spezifizierung beschrieben. Als Beispiel dazu kann hier der Fall angef&#252;hrt werden, wenn ein System nur Fr&#252;chte kennt, ein anderes aber &#196;pfel, Orangen und Bananen etc. unterscheiden kann. Ebenso k&#246;nnen Skalierungskonflikte auftreten, wenn etwa ein System &#8222;<i>Kind</i>&#8220; als Menschen im Alter zwischen 5 und 12 Jahren klassifiziert, ein anderes System zwischen 3 und 10 Jahren klassifiziert und ein drittes System &#8222;<i>Kind</i>&#8220; als Menschen unter 140 cm klassifiziert.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1434570244070" ID="ID_1069462239" MODIFIED="1434570251050" POSITION="right" TEXT="2. Ontologien">
+<node CREATED="1434570252438" ID="ID_1455475508" MODIFIED="1434650654197" TEXT="2.1 Begriff Ontologie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Der Begriff Ontologie (gr. &#243;ntos = Sein, l&#243;gos = Lehre) bezeichnet das Studium der Kategorisierung von Dingen, die existieren oder in einigen Interessensgebieten bestehen k&#246;nnen.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#8222;<i>An ontology is an explicit, formal specification of a shared conceptualization of a domain of interest.</i>&#8220; (c) Tom Gruber
+    </p>
+    <p>
+      - Fomal = maschinenlesbar
+    </p>
+    <p>
+      - shared = nicht privat, von der Gruppe akzeptiert
+    </p>
+    <p>
+      - domain of interest = nur relevante Teile
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Eine Ontologie stellt ein Vokabular zur Darstellung eines Wissensbereichs bereit, indem es Pr&#228;dikate verwendet, die die Beziehungen zwischen den Entit&#228;ten herstellen.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434650661415" FOLDED="true" ID="ID_76158491" MODIFIED="1434655027936" TEXT="2.2 Aufbau einer Ontologie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Das Problem der semantischen Heterogenit&#228;t kann reduziert werden, wenn alle Begriffe, die eine Ontologie beschreibt, m&#246;glichst eindeutig und nicht-redundant definiert werden und weltweit g&#252;ltig sind.
+    </p>
+    <p>
+      Eine Ontologie besteht aus mehreren Bestandteilen:
+    </p>
+    <p>
+      - Konzepte
+    </p>
+    <p>
+      - Instanzen
+    </p>
+    <p>
+      - Relationen
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434651390202" ID="ID_99581458" MODIFIED="1434651520972" TEXT="Konzepte">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Konzepte fassen real existierende Instanzen mit gemeinsamen Eigenschaften zusammen.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434651395793" ID="ID_890589884" MODIFIED="1434651526877" TEXT="Instanzen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Instanzen stellen die eigentlichen Objekte dar.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434651398970" ID="ID_1109015490" MODIFIED="1434651528284" TEXT="Relationen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Relationen verbinden Konzepte und Instanzen miteinander.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434652655052" ID="ID_1396218103" MODIFIED="1434652663150" TEXT="Beispiele f&#xfc;r Ontologien">
+<node CREATED="1434652666329" ID="ID_468757251" MODIFIED="1434652756653" TEXT="Taxonomie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eine Taxonomie ist eine der &#228;ltesten und wissenschaftlichen Disziplinen in der Medizin und der Biologie.
+    </p>
+    <p>
+      Mit Taxonomien (griech. Taxis = Ordnung, -nomia = Verwaltung) werden Gruppen von kontrollierten Vokabularien eines Themengebietes definiert und in eine hierarchische Beziehung gesetzt f&#252;r eine m&#246;glichst pr&#228;zise Beschreibung und Repr&#228;sentation eines Themengebiets. Es bestehen etwa internationale Regelwerke zur Nomenklatur f&#252;r die Zoologie (International Code of Zoological Nomenclature - ICZN).
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434653256941" ID="ID_1492220266" MODIFIED="1434653292336" TEXT="Schemata von DBS">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Schemata von Datenbanksystemen bilden eine konzeptuelle Datenbe-schreibung in Form von Entity-Relationship-Modellen oder UML-Diagrammen. Sie bieten eine Beschreibung von Begriffen und ihren Beziehungen an, die direkt in eine Ontologie umgesetzt werden k&#246;nnen.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434654257029" ID="ID_1747689556" MODIFIED="1434654372365" TEXT="FOAF Ontologie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Friend of a friend-&#160;&#160;ein Projekt zur maschinenlesbaren Modellierung sozialer Netzwerke.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1434655029488" ID="ID_270544401" MODIFIED="1434655737072" TEXT="2.3 Konzeptualisierung und geimensames Vokabular">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Zur Reduktion der semantischen Heterogenit&#228;t sind zwei Anforderungen zu stellen.
+    </p>
+    <p>
+      1. Einerseits ist die Einigung einer Gruppe von Anwendern auf die jeweiligen Begriffe und deren Zusammenh&#228;nge auf der Wissensebene n&#246;tig.
+    </p>
+    <p>
+      2. Andererseits ist die Einigung auf ein Vokabular n&#246;tig, das den Verweis auf die Begriffe und so die Repr&#228;sentation der Konzepte eindeutig identifiziert .
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434655743324" ID="ID_127003440" MODIFIED="1434744097320" TEXT="2.3.1 Konzeptualisierung">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ein Konzept ist eine gedankliche Vorstellung bzw. die subjektive Interpretation &#252;ber ein reales Ding in der Welt.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Eine Minimierung der ontologischen Zustimmung kann durch eine sehr schwache semantische Ontologie spezifiziert werden und nur die Begriffe eines Vokabulars werden dabei definiert, die unbedingt notwendig sind f&#252;r die Kommunikation. Das RDF-Konzept ist ein Beispiel f&#252;r eine schwach spezifizierte Theorie.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434655759467" ID="ID_1378753405" MODIFIED="1434744392752" TEXT="2.3.2 gemeinsames Vokabular">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RDF verwendet Uniform Resource Identifiers (URIs) zum Identifizieren der Ressourcen und verkn&#252;pft sie so mit einer eindeutigen Definition. Allerdings k&#246;nnen verschiedene RDF-Instanzen dieselben Sachverhalte in unterschied-licher Weise ausdr&#252;cken. Mit RDF-Schema (RDFS) kann ein Vokabular erstellt werden, das Gruppen von verwandten RDF-Ressourcen und ihre Beziehungen zwischen diesen Ressourcen auf einer h&#246;heren Abstraktionsebene flexibel beschreibt. Gleichzeitig bestehen RDFS-Triples aus Klassen, Klasseneigenschaften und Werten, sodass aufgrund von Typeigenschaften die Eindeutigkeit von Sachverhalten sichergestellt werden kann. Mit RDFS kann ein dom&#228;nenspezifisches Vokabular erstellt werden. Ein Abgleich von Aussagen verschiedener Dom&#228;nen erfordert noch zus&#228;tzliche Axiome wie etwa subClassOf, disjointWith und Klassenbeschreibungen wie unionOf, intersectionOf etc., die von der Ontologiewebsprache OWL bereitgestellt werden.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434745657811" ID="ID_694067719" MODIFIED="1434745661583" TEXT="RDF-Schema"/>
+<node CREATED="1434745661930" ID="ID_1804784508" MODIFIED="1434745667767" TEXT="RFD-Triples"/>
+<node CREATED="1434745669258" ID="ID_1160116904" MODIFIED="1434745673022" TEXT="Vokabularien"/>
+<node CREATED="1434745673954" ID="ID_452018699" MODIFIED="1434745682087" TEXT="Top-Level Ontologie"/>
+</node>
+</node>
+</node>
+</node>
+</map>
