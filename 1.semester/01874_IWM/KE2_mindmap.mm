7edb0c6b7f426d153adbca02c857b876bb83a149
Alexander <alexander.albrant@googlemail.com>
2015-06-21 22:28:04 +0200

    Einsende Aufgabe2 und mm

--- 1.semester/01874_IWM/KE2_mindmap.mm
+++ 1.semester/01874_IWM/KE2_mindmap.mm
@@ -13,8 +13,7 @@
       Bei semantisch &#228;quivalenten Konzepten k&#246;nnen sich etwa verschiedene Begriffe auf ein und dasselbe Konzept beziehen oder verschiedene Eigenschaften f&#252;r das gleiche Produkt von zwei Systemen modelliert werden.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434485173932" ID="ID_1260039672" MODIFIED="1434485323589" TEXT="sematische unabh&#xe4;ngige Konzepte">
 <richcontent TYPE="NOTE"><html>
@@ -26,8 +25,7 @@
       Bei semantisch unabh&#228;ngigen Konzepten kann es zu Verst&#228;ndnis-problemen kommen, wenn der gleiche Begriff f&#252;r unterschiedliche Konzepte verwendet wird. ls Beispiel kann &#8222;<i>K&#228;fer</i>&#8220; entweder f&#252;r ein &#8222;<i>Tier</i>&#8220; oder f&#252;r ein &#8222;<i>Auto</i>&#8220; stehen.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434485186954" ID="ID_1493538189" MODIFIED="1434485368704" TEXT="semantische abh&#xe4;ngige Konzepte">
 <richcontent TYPE="NOTE"><html>
@@ -39,8 +37,7 @@
       Bei semantisch abh&#228;ngigen Konzepten wird die Heterogenit&#228;t anhand von Generalisierung und Spezifizierung beschrieben. Als Beispiel dazu kann hier der Fall angef&#252;hrt werden, wenn ein System nur Fr&#252;chte kennt, ein anderes aber &#196;pfel, Orangen und Bananen etc. unterscheiden kann. Ebenso k&#246;nnen Skalierungskonflikte auftreten, wenn etwa ein System &#8222;<i>Kind</i>&#8220; als Menschen im Alter zwischen 5 und 12 Jahren klassifiziert, ein anderes System zwischen 3 und 10 Jahren klassifiziert und ein drittes System &#8222;<i>Kind</i>&#8220; als Menschen unter 140 cm klassifiziert.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -76,8 +73,7 @@
       Eine Ontologie stellt ein Vokabular zur Darstellung eines Wissensbereichs bereit, indem es Pr&#228;dikate verwendet, die die Beziehungen zwischen den Entit&#228;ten herstellen.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434650661415" FOLDED="true" ID="ID_76158491" MODIFIED="1434655027936" TEXT="2.2 Aufbau einer Ontologie">
 <richcontent TYPE="NOTE"><html>
@@ -101,8 +97,7 @@
       - Relationen
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1434651390202" ID="ID_99581458" MODIFIED="1434651520972" TEXT="Konzepte">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -113,8 +108,7 @@
       Konzepte fassen real existierende Instanzen mit gemeinsamen Eigenschaften zusammen.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434651395793" ID="ID_890589884" MODIFIED="1434651526877" TEXT="Instanzen">
 <richcontent TYPE="NOTE"><html>
@@ -126,8 +120,7 @@
       Instanzen stellen die eigentlichen Objekte dar.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434651398970" ID="ID_1109015490" MODIFIED="1434651528284" TEXT="Relationen">
 <richcontent TYPE="NOTE"><html>
@@ -139,8 +132,7 @@
       Relationen verbinden Konzepte und Instanzen miteinander.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434652655052" ID="ID_1396218103" MODIFIED="1434652663150" TEXT="Beispiele f&#xfc;r Ontologien">
 <node CREATED="1434652666329" ID="ID_468757251" MODIFIED="1434652756653" TEXT="Taxonomie">
@@ -156,8 +148,7 @@
       Mit Taxonomien (griech. Taxis = Ordnung, -nomia = Verwaltung) werden Gruppen von kontrollierten Vokabularien eines Themengebietes definiert und in eine hierarchische Beziehung gesetzt f&#252;r eine m&#246;glichst pr&#228;zise Beschreibung und Repr&#228;sentation eines Themengebiets. Es bestehen etwa internationale Regelwerke zur Nomenklatur f&#252;r die Zoologie (International Code of Zoological Nomenclature - ICZN).
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434653256941" ID="ID_1492220266" MODIFIED="1434653292336" TEXT="Schemata von DBS">
 <richcontent TYPE="NOTE"><html>
@@ -169,8 +160,7 @@
       Die Schemata von Datenbanksystemen bilden eine konzeptuelle Datenbe-schreibung in Form von Entity-Relationship-Modellen oder UML-Diagrammen. Sie bieten eine Beschreibung von Begriffen und ihren Beziehungen an, die direkt in eine Ontologie umgesetzt werden k&#246;nnen.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434654257029" ID="ID_1747689556" MODIFIED="1434654372365" TEXT="FOAF Ontologie">
 <richcontent TYPE="NOTE"><html>
@@ -182,8 +172,7 @@
       Friend of a friend-&#160;&#160;ein Projekt zur maschinenlesbaren Modellierung sozialer Netzwerke.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -203,8 +192,7 @@
       2. Andererseits ist die Einigung auf ein Vokabular n&#246;tig, das den Verweis auf die Begriffe und so die Repr&#228;sentation der Konzepte eindeutig identifiziert .
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1434655743324" ID="ID_127003440" MODIFIED="1434744097320" TEXT="2.3.1 Konzeptualisierung">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -221,8 +209,7 @@
       Eine Minimierung der ontologischen Zustimmung kann durch eine sehr schwache semantische Ontologie spezifiziert werden und nur die Begriffe eines Vokabulars werden dabei definiert, die unbedingt notwendig sind f&#252;r die Kommunikation. Das RDF-Konzept ist ein Beispiel f&#252;r eine schwach spezifizierte Theorie.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1434655759467" ID="ID_1378753405" MODIFIED="1434744392752" TEXT="2.3.2 gemeinsames Vokabular">
 <richcontent TYPE="NOTE"><html>
@@ -234,12 +221,243 @@
       RDF verwendet Uniform Resource Identifiers (URIs) zum Identifizieren der Ressourcen und verkn&#252;pft sie so mit einer eindeutigen Definition. Allerdings k&#246;nnen verschiedene RDF-Instanzen dieselben Sachverhalte in unterschied-licher Weise ausdr&#252;cken. Mit RDF-Schema (RDFS) kann ein Vokabular erstellt werden, das Gruppen von verwandten RDF-Ressourcen und ihre Beziehungen zwischen diesen Ressourcen auf einer h&#246;heren Abstraktionsebene flexibel beschreibt. Gleichzeitig bestehen RDFS-Triples aus Klassen, Klasseneigenschaften und Werten, sodass aufgrund von Typeigenschaften die Eindeutigkeit von Sachverhalten sichergestellt werden kann. Mit RDFS kann ein dom&#228;nenspezifisches Vokabular erstellt werden. Ein Abgleich von Aussagen verschiedener Dom&#228;nen erfordert noch zus&#228;tzliche Axiome wie etwa subClassOf, disjointWith und Klassenbeschreibungen wie unionOf, intersectionOf etc., die von der Ontologiewebsprache OWL bereitgestellt werden.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1434745657811" ID="ID_694067719" MODIFIED="1434745661583" TEXT="RDF-Schema"/>
 <node CREATED="1434745661930" ID="ID_1804784508" MODIFIED="1434745667767" TEXT="RFD-Triples"/>
 <node CREATED="1434745669258" ID="ID_1160116904" MODIFIED="1434745673022" TEXT="Vokabularien"/>
-<node CREATED="1434745673954" ID="ID_452018699" MODIFIED="1434745682087" TEXT="Top-Level Ontologie"/>
+<node CREATED="1434745673954" ID="ID_452018699" MODIFIED="1434913438341" TEXT="Top-Level Ontologie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Um eine Nachhaltigkeit beim Einsatz von Ontologien zu gew&#228;hrleisen, da die Erstellung einer Ontologie mit der Standardisierung der Terminologie sehr m&#252;hsam ist, haben sich verschiedene Top-Level-Ontologien entwickelt, die sehr allgemeine Konzepte beschreiben.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1434913475517" ID="ID_307756100" MODIFIED="1434913867564" TEXT="2.4 Spezifikation eines Wissenskontexts">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Spezifikation eines Wissenskontexts bedeutet, dass man sich gegenseitig darauf verst&#228;ndigen und einigen muss, welche Bedeutung die Entit&#228;ten haben, die den Inhalt einer Web-Informationsressource repr&#228;sentieren
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434913872354" ID="ID_1118654154" MODIFIED="1434914420430" TEXT="2.4.1 Grad der Formalit&#xe4;t">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Der Grad der Formalit&#228;t bestimmt den Grad der semantischen Interoperabilit&#228;t, der F&#228;higkeit zur Zusammenarbeit heterogener Informationssysteme nicht nur syntaktischer, sondern auch inhaltlicher Art
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434913886515" ID="ID_845234856" MODIFIED="1434914432158" TEXT="Total-informell">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eine Ontologie wird einfach in nat&#252;rlicher Sprache beschrieben, viele Glossare passen in diese Kategorie.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434913892489" ID="ID_595669774" MODIFIED="1434914437720" TEXT="Semi-informaell">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Ontologie wird in einer strukturierten Form mittels einer nat&#252;rlichen Sprache dargestellt, die Mehrdeutigkeiten reduziert. Die Taxonomie kann z. B. ein Wortschatz sein, der in einer Generalisierungs-Spezialisierungs-Hierarchie strukturiert ist.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434913898809" ID="ID_1065518043" MODIFIED="1434914441596" TEXT="Semi-formell">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eine semi-formale Sprache ist eine k&#252;nstlich formal definierte Sprache, die die Beschreibung der Begriffe sowohl informell in einer nat&#252;rlichen Sprache als auch formal in einer f&#252;r Computer interpretierbaren Wissensrepr&#228;sentationssprache unterst&#252;tzt .
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434914583253" ID="ID_1661482474" MODIFIED="1434915020195" TEXT="Regelsysteme">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eine Taxonomie, die mit einem Regelsystem implementiert ist, erm&#246;glicht die schnelle und einfache Darstellung von Wissen in Regeln.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434914587492" ID="ID_1755191562" MODIFIED="1434915049366" TEXT="Framekonzepte">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Durch die Kombination der in den Frames enthaltenen Informationseinheiten (Framename, Slots und Filler) wird Wissen &#252;ber die Konzepte zur Verf&#252;gung gestellt. In diesem Fall spricht man auch von Klassenkonzepte bzw. Klassenframes. Einzelframes werden als `Instanzen von Klassenframes&#180; bezeichnet.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Die Framekonzepte &#228;hneln dem Klassenkonzept der objekt-orientierten Programmierung (OOP)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434914599709" ID="ID_1133074622" MODIFIED="1434915082269" TEXT="Semantische Netzwerke">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      In Semantischen Netzwerken werden Konzepte als Knoten, Relationen als Kanten visualisiert. Der Vorteil liegt in der Widerspieglung der semantischen N&#228;he zwischen den Konzepten innerhalb eines semantischen Netzes.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1434914612148" ID="ID_346601537" MODIFIED="1434915205966" TEXT="Rigoros Formal">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eine auf der Pr&#228;dikatenlogik basierende formale Sprache treibt den Standardisierungsprozess voran und erm&#246;glicht die Entwicklung von Schlussfolgerungskalk&#252;len, der g&#252;ltige Eigenschaften von Mengen von Ausdr&#252;cken ableitet, sodass Fragen der Vollst&#228;ndigkeit und Korrektheit von formalen Spezifikationen &#252;berpr&#252;ft werden k&#246;nnen. Beschrei-bungslogiken (description logics) sind eine Familie von Sprachen zur Wissensrepr&#228;sentation. Die meisten Beschreibungslogiken sind eine Untermenge der Pr&#228;dikatenlogik erster Stufe, im Gegensatz zu dieser aber entscheidbar. Sie basieren auf dem Open World Prinzip, das einen Mangel an Wissen nicht unbedingt falsifiziert und negiert. Sie gehen von einer Information aus, die unvollst&#228;ndig spezifiziert ist. Das Open World Prinzip ist daher gut geeignet f&#252;r die Beschreibung von Wissen, das sich iterativ erweitern kann [20]. KIF (Knowledge Interchange Format) ist entwickelt worden, um verschiedenen wissensbasierten Systemen den Wissensaustausch zu erm&#246;glichen. Es basiert auf dem Pr&#228;dikatenkalk&#252;l.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1434914624820" ID="ID_832928447" MODIFIED="1434916378131" TEXT="2.4.2 Ausma&#xdf; der Definitionstiefe">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Das Ausma&#223; der Definitionstiefe ist von dem Zweck abh&#228;ngig, die eine Ontologie erf&#252;llen soll.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Im Folgenden werden kurz Webstandards vorgestellt, die Wissen implementierungsabh&#228;ngig von einer Programmiersprache darstellen. Die Sprachen werden anhand der Ausdrucksst&#228;rke, der Inferenzm&#228;chtigkeit und der Entscheidbarkeit miteinander verglichen
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434916328872" ID="ID_1192736291" MODIFIED="1434916404913" TEXT="XML">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      stellt eine Syntax f&#252;r strukturierte Dokumente zur Verf&#252;gung, nutzt aber keine semantische Eingrenzung der Bedeutung dieser Dokumente.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434916334351" ID="ID_85744312" MODIFIED="1434916416667" TEXT="SML-Schema">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ist die Sprache, um die Struktur von XML-Dokumenten festzulegen und erweitert XML auch um die Datentypen.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434916338984" ID="ID_215806790" MODIFIED="1434916417833" TEXT="RDF">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ist ein Datenmodell f&#252;r Ressourcen und Relationen zwischen diesen. Es stellt eine einfache Semantik f&#252;r das Datenmodell zur Verf&#252;gung, die in XML repr&#228;sentiert werden kann.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434916345320" ID="ID_557460109" MODIFIED="1434916534224" TEXT="RDT-Schema">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Schema stellt ein Vokabular zur Beschreibung der Eigenschaften und Klassen von RDF-Ressourcen mit einer Semantik f&#252;r verallgemeinernde Hierarchien solcher Eigenschaften und Klassen bereit.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434916351599" ID="ID_1886846574" MODIFIED="1434916635466" TEXT="DAML+OIL">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DAML-ONT (Darpa Agent Markup Language-Ontology) und OIL (Ontology Inference Layer) flie&#223;en in DAML+OIL zusammen. Die Beschreibungssprache kann als eine Ontologie-Infrastruktur f&#252;r das SemanticWeb betrachtet werden. OIL basiert auf Konzepten, die in Beschreibungslogik und frame-basierten Systemen entwickelt worden sind. OIL ist kompatibel mit RDFS. Sie ist somit entscheidbar und unterst&#252;tzt effizientes Reasoning
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434916360744" ID="ID_1959391930" MODIFIED="1434916656945" TEXT="OWL">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Web Ontology Language ist eine Spezifikation des W3C und basiert historisch auf DAML+OIL. Sie f&#252;gt mehr Vokabeln zur Beschreibung von Eigenschaften und Klassen hinzu (Disjunktheit, Kardinalit&#228;t, &#196;quivalenz etc). Innerhalb der OWL-Familie kann man drei Dialekte mit zunehmender Ausdrucksst&#228;rke unterscheiden:
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </node>
